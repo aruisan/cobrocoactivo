982c7d86581a9cfe3e94a49b7a9890973f31d9b5
Deivithsan <deivith.1@hotmail.com>
2018-12-03 09:16:30 -0500

    cambio a migracion para poder realizar el rollback

--- database/migrations/2018_11_27_041852_update_fonts_rubro_table.php
+++ database/migrations/2018_11_27_041852_update_fonts_rubro_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateFontsRubroTable extends Migration
+class UpdateFontsRubroTable2 extends Migration
 {
     /**
      * Run the migrations.
