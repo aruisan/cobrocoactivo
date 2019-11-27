fb7fdaaabd52736ba39fb36bc086a2d625e9a0c4
DeivithB <deivith.1@hotmail.com>
2019-11-27 16:17:17 -0500

    arreglo de validacion en homologar - Botones no se veian en los usuarios de tipo distinto a secretaria

--- resources/views/hacienda/presupuesto/informes/Contractual/Homologar/index.blade.php
+++ resources/views/hacienda/presupuesto/informes/Contractual/Homologar/index.blade.php
@@ -3,7 +3,6 @@
     Homologar
 @stop
 @section('sidebar')
-    @if( $rol == 2)
         <li>
             <a href="{{ url('presupuesto/informes/contractual/homologar/create') }}" class="btn btn-success">
                 <i class="fa fa-plus"></i>&nbsp;
@@ -14,7 +13,6 @@
                 <i class="fa fa-plus"></i>&nbsp;
                 <span class="hide-menu"> Asignar Código Contractual</span></a>
         </li>
-    @endif
 @stop
 @section('content')
     <div class="col-md-12 align-self-center">
@@ -23,10 +21,11 @@
                 <h4><b>Homologar</b></h4>
             </strong>
         </div>
-        <div class="table-responsive">
-            <table class="table table-bordered hover" id="tabla">
-                <hr>
-                <thead>
+        @if(isset($Rubros))
+            <div class="table-responsive">
+                <table class="table table-bordered hover" id="tabla">
+                    <hr>
+                    <thead>
                     <tr>
                         <th colspan="5" class="text-center">Tabla de Rubros con sus Códigos Contractuales Asignados</th>
                     </tr>
@@ -37,42 +36,56 @@
                         <th class="text-center">Valor Inicial</th>
                         <th class="text-center">Valor Disponible</th>
                     </tr>
-                </thead>
-                <tbody>
-                @foreach($Rubros as $value)
-                    <tr>
-                        <td class="text-center">{{$value['codigo']}}</td>
-                        <td class="text-center">{{$value['rubro']}}</td>
-                        <td class="text-center">{{$value['name']}}</td>
-                        <td class="text-center">$ <?php echo number_format($value['valor'],0);?></td>
-                        <td class="text-center">$ <?php echo number_format($value['valor_disp'],0);?></td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            <br>
-            <hr>
-            <br>
-            <table class="table table-bordered hover" id="tabla2">
-                <thead>
-                <tr>
-                    <th colspan="5" class="text-center">Códigos Contractuales</th>
-                </tr>
-                <tr>
-                    <th class="text-center">Id</th>
-                    <th class="text-center">Código</th>
-                    <th class="text-center">Nombre</th>
-                    <th class="text-center">Estado</th>
-                    <th class="text-center">Editar</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($codes as $value)
-                    <tr>
-                        <td class="text-center">{{$value->id}}</td>
-                        <td class="text-center">{{$value->code}}</td>
-                        <td class="text-center">{{$value->name}}</td>
-                        <td class="text-center">
+                    </thead>
+                    <tbody>
+                    @foreach($Rubros as $value)
+                        <tr>
+                            <td class="text-center">{{$value['codigo']}}</td>
+                            <td class="text-center">{{$value['rubro']}}</td>
+                            <td class="text-center">{{$value['name']}}</td>
+                            <td class="text-center">$ <?php echo number_format($value['valor'],0);?></td>
+                            <td class="text-center">$ <?php echo number_format($value['valor_disp'],0);?></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                @else
+                    <br><br>
+                    <div class="alert alert-danger">
+                        <center>
+                            Ningun rubro tiene asignado un código contractual, se recomienda hacerlo.
+                            <br>
+                            <br>
+                            <a href="{{ url('presupuesto/informes/contractual/asignar') }}" class="btn btn-success">
+                                <i class="fa fa-plus"></i>&nbsp;
+                                <span class="hide-menu"> Asignar Código Contractual</span></a>
+                        </center>
+                    </div>
+                @endif
+                <br>
+                <hr>
+                <br>
+                @if($codes->count() != 0)
+                    <table class="table table-bordered hover" id="tabla2">
+                        <thead>
+                        <tr>
+                            <th colspan="5" class="text-center">Códigos Contractuales</th>
+                        </tr>
+                        <tr>
+                            <th class="text-center">Id</th>
+                            <th class="text-center">Código</th>
+                            <th class="text-center">Nombre</th>
+                            <th class="text-center">Estado</th>
+                            <th class="text-center">Editar</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($codes as $value)
+                            <tr>
+                                <td class="text-center">{{$value->id}}</td>
+                                <td class="text-center">{{$value->code}}</td>
+                                <td class="text-center">{{$value->name}}</td>
+                                <td class="text-center">
                             <span class="badge badge-pill badge-danger">
                                 @if($value->estado == "0")
                                     Activado
@@ -80,15 +93,26 @@
                                     Desactivado
                                 @endif
                             </span>
-                        </td>
-                        <td class="text-center">
-                            <a href="{{ url('presupuesto/informes/contractual/homologar/'.$value->id.'/edit') }}" title="Editar" class="btn-sm btn-primary"><i class="fa fa-edit"></i></a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+                                </td>
+                                <td class="text-center">
+                                    <a href="{{ url('presupuesto/informes/contractual/homologar/'.$value->id.'/edit') }}" title="Editar" class="btn-sm btn-primary"><i class="fa fa-edit"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <br><br>
+                    <div class="alert alert-danger">
+                        <center>
+                            No hay códigos contractuales almacenados en el software, se recomienda hacerlo. <br><br>
+                            <a href="{{ url('presupuesto/informes/contractual/homologar/create') }}" class="btn btn-success">
+                                <i class="fa fa-plus"></i>&nbsp;
+                                <span class="hide-menu"> Añadir Código Contractual</span></a>
+                        </center>
+                    </div>
+                @endif
+            </div>
     </div>
 @stop
 
