831bc96eec311c28cd14f392dcbac7dd3e492615
aruisan <oskrjag@gmail.com>
2019-12-12 01:34:48 -0500

    se arreglo bug

--- database/migrations/2019_12_04_143615_create_fonts_vigencia_table.php
+++ database/migrations/2019_12_04_143615_create_fonts_vigencia_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FontsVigenciaTable extends Migration
+class CreateFontsVigenciaTable extends Migration
 {
     /**
      * Run the migrations.
